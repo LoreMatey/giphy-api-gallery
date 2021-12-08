4e66fe334a1fa4c19d3cccabfee710864df46c2e
LoreMatey <lorena.matey.l@gmail.com>
2021-12-09 00:54:06 +0100

    Created initial array of gifs as initial state and map function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const arrayOfGifs = [
+  'https://media4.giphy.com/media/l2JhGk8rR9WFLEDQY/giphy.webp?cid=ecf05e47hwugebj9fybnmgoaxq9qpnqv0ldz18lkyk1b9uee&rid=giphy.webp&ct=g',
+
+  'https://media0.giphy.com/media/l41lFj8afmWIo3yW4/200w.webp?cid=ecf05e47hwugebj9fybnmgoaxq9qpnqv0ldz18lkyk1b9uee&rid=200w.webp&ct=g'
+]
+
 function App() {
+  const [gifs, setGifs] = useState(arrayOfGifs)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        {
+          gifs.map(gif => 
+            <img src={gif} />)
+        }
+        
+      </section>
     </div>
   );
 }
